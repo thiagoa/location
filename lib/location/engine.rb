eb9eecde73b25471d2c6c1499e7a9df4ac6a8fdd
Thiago A. Silva <thiago@mixinternet.com.br>
2013-12-10 14:02:49 -0300

    Setup factory_girl and Guard

--- lib/location/engine.rb
+++ lib/location/engine.rb
@@ -3,7 +3,8 @@ module Location
     isolate_namespace Location
 
     config.generators do |g|
-      g.test_framework :rspec, view_specs: false
+      g.test_framework :rspec, view_specs: false, fixture_replacement: :factory_girl
+      g.fixture_replacement :factory_girl, dir: 'spec/support/factories'
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,11 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'shoulda/matchers'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
