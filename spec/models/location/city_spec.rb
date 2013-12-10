599e21e11f62be6cc810cd54d62c520c24d6577f
Thiago A. Silva <thiago@mixinternet.com.br>
2013-12-10 14:18:58 -0300

    Add tests for inverse relationships

--- spec/models/location/city_spec.rb
+++ spec/models/location/city_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 module Location
   describe City do
     it { should belong_to :state }
+    it { should have_many :districts }
   end
 end
--- spec/models/location/district_spec.rb
+++ spec/models/location/district_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 module Location
   describe District do
     it { should belong_to(:city) }
+    it { should have_many(:addresses) }
   end
 end
