ea46222cbc2e627b9f7501fd1c22ae9a80cd2360
Thiago A. Silva <thiagoaraujos@gmail.com>
2015-07-14 00:08:03 -0300

    Change FinderController#show to render JSON, not JS anymore

--- app/controllers/location/finder_controller.rb
+++ app/controllers/location/finder_controller.rb
@@ -1,10 +1,9 @@
 module Location
   class FinderController < ActionController::Base
-    respond_to :js
-
     def show
       Finder.find params[:postal_code] do |f|
-        @address = f.address.to_hash(only: params[:only]).to_json
+        address = f.address.to_hash(only: params[:only]).to_json
+        render json: address
       end
     end
   end
--- app/views/location/finder/show.js.coffee
+++
@@ -1,6 +0,0 @@
-address = jQuery.parseJSON('<%= raw @address %>')
-postal_code_id = '#<%= params[:postal_code_id] %>'
-prefix = postal_code_id.replace /postal_code$/, ''
-
-for attr, value of address
-  $("#{prefix}#{attr}").val(value) if value
